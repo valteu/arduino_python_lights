de9252df3b1c496846323cc71429bc31164e8a6a
valteu <62844981+valteu@users.noreply.github.com>
2021-04-03 22:50:05 +0200

    Update pyrhon_serial_tkinter_code_1.1.py

--- python_code/pyrhon_serial_tkinter_code_1.1.py
+++ python_code/pyrhon_serial_tkinter_code_1.1.py
@@ -1,142 +1,190 @@
-import serial
-import time
-import tkinter as tk
-from tkinter import *
-import os
+String data;
+String mode;
+double green;
+double red;
+double blue;
+String num_led;
+int brightness;
 
-root = tk.Tk()
+#include <FastLED.h>
 
-def light_off():
-	data_to_arduino(mode=0, num_led=0, brightness=0)
-
-def static_green():
-	data_to_arduino(mode=1, num_led=12, brightness=100)
-
-def static_yellow():
-	data_to_arduino(mode=2, num_led=12, brightness=100)
-
-def static_red():
-	data_to_arduino(mode=3, num_led=12, brightness=100)
-
-def static_pink():
-	data_to_arduino(mode=4, num_led=12, brightness=100)
-
-def static_tortoise():
-	data_to_arduino(mode=5, num_led=12, brightness=100)
-
-def static_blue():
-	data_to_arduino(mode=6, num_led=12, brightness=100)
-
-def static_white():
-	data_to_arduino(mode=7, num_led=12, brightness=100)
-
-def rainbow_chase():
-	data_to_arduino(mode=10, num_led=12, brightness=100)
-
-def fade():
-	data_to_arduino(mode=11, num_led=12, brightness=100)
-
-def single_ball():
-	data_to_arduino(mode=12, num_led=12, brightness=100)
-
-def file_menu_command():
-	print("opned home menu")
-
-def settings_command():
-	settings_frame.pack(fill="both", expand=1)
-
-def hide_all_frames():   
-	settings_frame.pack_forhet()
-
-main_menu = Menu(root)
-root.config(menu=main_menu)
-
-file_menu = Menu(main_menu)
-main_menu.add_cascade(label="home", menu=file_menu)
-file_menu.add_command(label="New", command=file_menu_command)
-file_menu.add_separator()
-file_menu.add_command(label="exit", command=root.quit)
-
-setting_menu = Menu(main_menu)
-main_menu.add_cascade(label="Settings", menu=setting_menu)
-setting_menu.add_command(label="New", command=settings_command)
+#define NUM_LEDS 12      /* The amount of pixels/leds you have */
+#define DATA_PIN 3       /* The pin your data line is connected to */
+#define LED_TYPE WS2812B /* I assume you have WS2812B leds, if not just change it to whatever you have */
 
 
-settings_frame = Frame(root, width=400, height=400, bg="red")
+CRGB leds[NUM_LEDS];
+
+void setup() { 
+  FastLED.clear();
+  Serial.begin(9600); //initialize serial COM at 9600 baudrate
+  pinMode(LED_BUILTIN, OUTPUT); //make the LED pin (13) as output
+  FastLED.addLeds<LED_TYPE, DATA_PIN, RGB>(leds, NUM_LEDS);
+  Serial.println("Hi!, I am Arduino");
+}
+ 
+void loop() {
+  Serial.println("1.dsdsa");
+  if (Serial.available()){
+    Serial.println("2.dsdsa");
+    for (int i = 0; i < 6; i++)
+    {
+      Serial.println("3.dsdsa");
+      data = Serial.read();
+      if (i == 0)
+      {
+        mode = data;
+      }
+      else if (i == 1){
+        green = data.toDouble();
+      }
+      else if (i == 2)
+      {
+        red = data.toDouble();
+      }
+      else if (i == 3)
+      {
+        blue = data.toDouble();
+      }
+      else if (i == 4)
+      {
+        num_led = data;
+      }
+      else if (i == 5)
+      {
+        brightness = data.toInt();
+      }
+    }
+    Serial.println(mode);
+    Serial.println(num_led);
+    Serial.println(brightness);
+  modes(mode, green/100, red/100, blue/100, brightness);
+  }
+}
 
 
-canvas = tk.Canvas(root, height=1080, width=1920, bg="#590022")
-canvas.pack()
+String modes(String mode, double green, double red, double blue, int brightness){
 
-# frame = tk.Frame(root, bg="grey")
-# frame.place(relwidth=0.9, relheight=0.725, relx=0.05, rely=0.15)
-
-Off = tk.Button(root, text="Off", padx=10, pady=5, fg="white", bg="#263D42", command=light_off)
-Off.pack()
-Off.place(x=25, y=100)
-
-StaticGreen = tk.Button(root, text="Static Green", padx=10, pady=5, fg="white", bg="#263D42", command=static_green)
-StaticGreen.pack()
-
-StaticYellow = tk.Button(root, text="Static Yellow", padx=10, pady=5, fg="white", bg="#263D42", command=static_yellow)
-StaticYellow.pack()
-
-StaticRed = tk.Button(root, text="Static Red", padx=10, pady=5, fg="white", bg="#263D42", command=static_red)
-StaticRed.pack()
-
-StaticPink = tk.Button(root, text="Static Pink", padx=10, pady=5, fg="white", bg="#263D42", command=static_pink)
-StaticPink.pack()
-
-StaticTortoise = tk.Button(root, text="Static Tortoise", padx=10, pady=5, fg="white", bg="#263D42", command=static_tortoise)
-StaticTortoise.pack()
-
-StaticBlue = tk.Button(root, text="Static Blue", padx=10, pady=5, fg="white", bg="#263D42", command=static_blue)
-StaticBlue.pack()
-
-StaticWhite = tk.Button(root, text="Static White", padx=10, pady=5, fg="white", bg="#263D42", command=static_white)
-StaticWhite.pack()
-
-RainbowChase = tk.Button(root, text="Rainbow Chase", padx=10, pady=5, fg="white", bg="#263D42", command=rainbow_chase)
-RainbowChase.pack()
-
-Fade = tk.Button(root, text="Fade", padx=10, pady=5, fg="white", bg="#263D42", command=fade)
-Fade.pack()
-
-SingleBall = tk.Button(root, text="Singel Ball", padx=10, pady=5, fg="white", bg="#263D42", command=single_ball)
-SingleBall.pack()
-
-# headline = tk.Text(root, height=1, width=12, padx=50, pady=50)
-# headline.i nsert(tk.END, "Valteu lights")
-# headline.config(font=("Modern", 44))
-
-# headline.pack()
-# headline.place(x=1920/3, y=1080/8)
-
-ArduinoSerial = serial.Serial('/dev/ttyUSB0',9600)
-print(ArduinoSerial.name)
-time.sleep(2) #wait for 2 secounds for the communication to get established
-
-data = [0, 0, 0]
-
-print(ArduinoSerial.readline()) #read the serial data and print it as line
-
-
-
-def data_to_arduino(mode, num_led, brightness):
-		
-    # mode = input("Color/ effect: ")
-    data[0] = int(mode)
-    # num_led = input("Number of leds: ")
-    data[1] = int(num_led)
-    # brightness = input("brightness: ")
-    data[2] = int(brightness)
-    data_array = bytearray(data)
-
-
-    print("you entered", mode) #print the intput for confirmation
-    
-    ArduinoSerial.write(data_array)
-
-    time.sleep(1)
-
-root.mainloop()
+  if (mode == "0"){  /* off */
+    FastLED.clear();
+    Serial.println(mode);
+    while (mode == "0"){
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i].setRGB(0, 0, 0);/* green, red, blue */
+        FastLED.show();
+        
+      }
+      if (Serial.available() > 0){
+          loop();
+        }
+      else{
+        ;
+       }
+    }
+  }
+  else if (mode == "1"){ /* static */
+    FastLED.clear();
+    while (mode == "1"){
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i].setRGB((green * brightness), (red * brightness), (blue * brightness));
+        FastLED.show();
+      }
+      if (Serial.available() > 0){
+          loop();
+        }
+      else{
+        ;
+       }
+    }
+  }                  /* light effects */
+  else if (mode == "10"){ /* rainbow */
+    FastLED.clear();
+    while (mode == "10"){
+      for (int j = 0; j < 255; j++) {
+        if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+        for (int i = 0; i < NUM_LEDS; i++) {
+          leds[i] = CHSV(i - (j * 2), 255, brightness); /* The higher the value 4 the less fade there is and vice versa */ 
+        }
+        FastLED.show();
+        delay(25); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
+        }
+     if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+     }
+    }
+  else if (mode == "11"){ /* fade */
+    FastLED.clear();
+    while (mode == "11"){
+    for( int colorStep=0; colorStep<256; colorStep++ ) {
+  
+        int r = colorStep;  // Redness starts at zero and goes up to full
+        int b = 255-colorStep;  // Blue starts at full and goes down to zero
+        int g = colorStep -255;  // No green needed to go from blue to red
+  
+        for(int x = 0; x < NUM_LEDS; x++){
+            leds[x] = CRGB(r,g,b);
+            if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+        }
+        FastLED.show();
+        delay(10);
+      if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+      }
+    }
+  }
+  else if (mode == "12"){ /* ball */
+    FastLED.clear();
+    while (mode == "12"){
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Red;
+        FastLED.show();
+        delay(100);
+        leds[i] = CRGB::Black;
+        if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+      }
+      for (int i = NUM_LEDS - 1; i >= 0; i--)
+      {
+        leds[i] = CRGB::Red;
+        FastLED.show();
+        delay(100);
+        leds[i] = CRGB::Black;
+        if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+      }
+      if (Serial.available() > 0){
+          loop();
+        }
+        else{
+          ;
+         }
+    }
+  }
+}
